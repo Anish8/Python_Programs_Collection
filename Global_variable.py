c46f29f11d02457540f3ea39314ad41818789a58
Anish8 <anishsubedi9@gmail.com>
2020-03-17 19:25:59 +0545

    Work with global Scope varibale

---
+++ Global_variable.py
@@ -0,0 +1,15 @@
+# creating global variable and make changes to the vaatibale in the lovl context
+def foo():
+    x = 20
+
+    def bar():
+        global x
+        x = 25
+    
+    print("Before calling bar: ", x)
+    print("Calling bar now")
+    bar()
+    print("After calling bar: ", x)
+
+foo()
+print("x in main : ", x)
